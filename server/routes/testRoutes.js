7dd910550d91eaac364c56f2ff078b0c275d3274
deralph <jraphael441@gmail.com>
2022-11-16 06:58:38 -0800

    created a  route for updating of recipe

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { CheckTest } = require("../controllers/testController");
+const updateRecipe = require("../controllers/updateRecipe");
 const { getAllRecipes } = require("../controllers/getAllRecepieController");
 const { Authenticate } = require("../middlewares/authMiddleware");
 
@@ -8,5 +9,8 @@ const router = express.Router();
 // use the Authenticate middleware to protect routes
 router.get("/", getAllRecipes);
 router.post("/greet", Authenticate, CheckTest);
+
+// route to update recipe
+router.patch("/recipe/:id", Authenticate, updateRecipe);
 
 module.exports = router;
