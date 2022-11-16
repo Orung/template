1cc6b2ffacd870cd8e0106dd4702f24316a36a47
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2022-11-16 14:45:27 +0100

    Updated

---
+++ server/models/Ingredients.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ingredientSchema = new Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	image: {
+		type: String,
+		required: true
+    }
+},
+                                    {timestamps: true });
+
+const Ingredient = mongoose.model('Ingredient', ingredientSchema);
+module.exports = Ingredient;
