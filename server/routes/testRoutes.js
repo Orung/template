fc49b8c74383d71f4f9ab88ede559d9928b15afa
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2022-11-16 13:26:41 +0100

    ft: add get single recipe api

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { getSingleRecipe } = require('../controllers/getSingleRecipeController')
 const { CheckTest } = require('../controllers/testController')
 const { Authenticate } = require('../middlewares/authMiddleware')
 
@@ -8,7 +9,8 @@ const router = express.Router()
 // use the Authenticate middleware to protect routes
 router.post('/greet', Authenticate, CheckTest)
 
-
+// get a single recipe route
+router.get('/recipe:id', getSingleRecipe)
 
 module.exports = router
 
