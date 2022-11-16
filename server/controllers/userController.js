1fea616b357b2f775087da4b381dd28529daa57c
Kriskazy <donkris93@gmail.com>
2022-11-16 12:36:32 +0100

    update api ednpoint

---
+++ server/controllers/userController.js
@@ -0,0 +1,27 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+
+exports.updateProfile = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      user.name = name || user.name;
+      user.email = email || user.email;
+      user.password = password || user.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } catch (error) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
